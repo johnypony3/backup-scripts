14c336715bda0762c2a6cfd735eff7484a71b6b9
johnypony3 <edward@theemm.com>
2022-09-06 02:42:40 -0700

    Emmmb2 (#2)
    * converted to borgmatic and added dev box backup

--- backup_emmmbp2.sh
+++ backup_emmmbp2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+cd  /Users/edward/git/backup-scripts
+
+export BORG_PASSPHRASE=$(</users/edward/.config/secrets/emmmb2-borg-key)
+
 borgmatic -c emmmbp2/borgmatic.yaml
--- backup_mediaserver.sh
+++ backup_mediaserver.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+cd /home/edward/backup-scripts
+
+export BORG_PASSPHRASE=$(</home/edward/.config/secrets/mediaserver-borg-key)
+
 borgmatic -c mediaserver/borgmatic.yaml
