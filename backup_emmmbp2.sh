3ad28e9fc0de797c8e2b1ee987495fcd245533ea
ponyboy3 <edward@theemm.com>
2022-09-06 03:19:17 -0700

    add borg alias

--- backup_emmmbp2.sh
+++ backup_emmmbp2.sh
@@ -4,4 +4,5 @@ cd  /Users/edward/git/backup-scripts
 
 export BORG_PASSPHRASE=$(</users/edward/.config/secrets/emmmb2-borg-key)
 
+alias borg=/opt/homebrew/bin/borg
 borgmatic -c emmmbp2/borgmatic.yaml
